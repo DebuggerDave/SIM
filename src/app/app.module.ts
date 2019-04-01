e8d5500fae01ff240fb97bf072a22b48f4d30e11
David Schwartz <dmschwartz@bis.midco.net>
2019-04-01 14:16:59 -0500

    Merge fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { GameHUDComponent } from './game-hud/game-hud.component';
-<<<<<<< HEAD
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
-=======
 import { HomepageComponent } from './homepage/homepage.component';
 import { RegistrationComponent } from './registration/registration.component';
->>>>>>> ab0fa5cf88d39b8784a91b83f770a2932c0422bd
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
     GameHUDComponent,
-<<<<<<< HEAD
-    LeaderboardComponent
-=======
+    LeaderboardComponent,
     HomepageComponent,
     RegistrationComponent
->>>>>>> ab0fa5cf88d39b8784a91b83f770a2932c0422bd
   ],
   imports: [
     BrowserModule,
