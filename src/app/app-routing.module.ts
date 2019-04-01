ab0fa5cf88d39b8784a91b83f770a2932c0422bd
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-04-01 13:39:25 -0500

    Changed routing so default path routes to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameHUDComponent } from './game-hud/game-hud.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/game', pathMatch: 'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomepageComponent },
   { path: 'game', component: GameHUDComponent }
 ];
 
