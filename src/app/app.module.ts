7d0226d944140b8bb47fb3376629d39988b0c2f8
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-04-01 13:28:38 -0500

    generated base registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { GameHUDComponent } from './game-hud/game-hud.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
     GameHUDComponent,
-    HomepageComponent
+    HomepageComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
