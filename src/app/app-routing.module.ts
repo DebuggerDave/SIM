892208a625591b89f2c6d516bbb0df93c72e4d95
Steven Karschnia <steven.karschnia@ndsu.edu>
2019-04-17 10:52:02 -0500

    added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameHUDComponent } from './game-hud/game-hud.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomepageComponent },
-  { path: 'game', component: GameHUDComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: HomepageComponent },
+  { path: 'game', component: GameHUDComponent },
+  { path: 'register', component: RegistrationComponent}
 ];
 
 @NgModule({
